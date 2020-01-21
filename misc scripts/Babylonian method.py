ea6fe35a33a42da58f3913ecffcf7de6897fce7f
chrisrutherford <chris36787@gmail.com>
2020-01-21 12:21:35 -0500

    updating older files

--- misc scripts/Babylonian method.py
+++ misc scripts/Babylonian method.py
@@ -1,18 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-x,y,a=300,10,6
 
 from math import sqrt
-def bab(x,y,a):
-    '''estimates sqrt(x) with y iterations of babylonian method
-    with a as initial estimate of sqrt(x)'''
-    
-    print("estimating square root of",x)
-    print("running through method",y,"times")
-    print("initial estimate:",a)
-    
-    for i in range(y):
-        a=(x/a+a)/2
-    print("approximate error:",str(sqrt(x)-a))
-    return a
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns; sns.set()
+
+number_to_estimate = float(input("Which number would you like to estimate the square root of? "))
+iterations = int(input("How many times should it iterate? "))
+initial_estimate = float(input("What is your estimate of the square root of {}? ".format(number_to_estimate)))
+x = [initial_estimate]
+error = [np.nan]
+
+print("Estimating square root of", number_to_estimate)
+print("Running through method", iterations, "times")
+print("Initial estimate:", initial_estimate)
+
+
+for i in range(iterations):
+    initial_estimate = ((number_to_estimate / initial_estimate) + initial_estimate) / 2
+    x.append(initial_estimate)
+    error.append(sqrt(number_to_estimate) - initial_estimate)
+
+print("Actual square root: {}".format(sqrt(number_to_estimate)))
+print("Approximate error (to about 15 decimal places):", str(abs(sqrt(number_to_estimate) - initial_estimate)))
+
+fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2)
+ax1.plot(x)
+ax1.legend(['Estimate'],loc='upper right')
+ax2.plot(error)
+ax2.legend(['Error'],loc='upper right')
+plt.suptitle("Estimation of square root of "+str(number_to_estimate))
+plt.show()
--- misc scripts/Law of Large Numbers.py
+++ misc scripts/Law of Large Numbers.py
@@ -1,61 +1,60 @@
 ##Law of Large Numbers
 
-from numpy.random import randn  ##RNG for normal distribution
-from scipy.stats import norm    ##calculate area under normal pdf
+from numpy.random import randn  # normal distribution random sampler
+from scipy.stats import norm  # calculate area under normal pdf
 import matplotlib.pyplot as plt
-import matplotlib.mlab as mlab
 import numpy as np
 
-def LLN(n,mu,sigma,sdRange):
-    '''law of large numbers with normal distribution'''
-    sample=mu+sigma*randn(n)    ##creates the desired sample
-    count=0                     ##counts values within desired range
-    largest=max(sample)
-    smallest=min(sample)
-    
-    for i in sample:
-        if not (i-sdRange*sigma>mu or i+sdRange*sigma<mu):
-            count+=1
-        if i>largest:
-            largest=i
-        if i<smallest:
-            smallest=i
-            
-    cdfRange=sigma*sdRange              ##used to calculate desired area
-                                        #under normal curve
-    
-    actPercent=str((count/n)*100)+"%"   ##actual percent of desired
-                                        #samples within specified range
-    
+n = int(input("Enter the desired sample size: "))
+mu = int(input("Enter the desired mean: "))
+sigma = int(input("Enter the desired standard deviation: "))
+sd_range = int(input("Enter the desired number of standard deviations away from the mean: "))
 
-    #theoretical percent of desired samples within specified range
-    expPercent=str((norm(mu, sigma).cdf(mu+cdfRange)-norm(mu, sigma).cdf(mu-cdfRange))*100)+"%"
-    
-    print("Total sample size:",n)
-    print("Total desired values:",count)
-    print("Largest value:",largest)
-    print("Smallest value:",smallest)
-    print("Expected percent of values within",str(sdRange),"sd of the mean:",expPercent)
-    print("Actual percent of values within",str(sdRange),"sd of the mean:",actPercent)
+'''law of large numbers with normal distribution'''
+sample = mu + sigma * randn(n)  # creates the desired sample
+count = 0  # counts values within desired range
+largest = max(sample)
+smallest = min(sample)
 
-    n, bins, patches=plt.hist(sample, 100, normed=1, facecolor='b',alpha=0.7)
-    (sampleMean,sampleVar)=norm.fit(sample)
-    y=mlab.normpdf(bins, sampleMean, sampleVar)
-    l=plt.plot(bins, y, linewidth=2, color='red')
-    plt.xlabel("Value")
-    plt.ylabel("Probability")
-    plt.title("Histogram of Sample: $\overline{x}=%.3f,\ s=%.3f$"%(sampleMean,sampleVar))
-    plt.xlim([mu-4*sigma, mu+4*sigma])
-    plt.grid(True)
-    
-    ##lines for theoretical range for given sd range
-    plt.axvline(x=mu-cdfRange,c='black',linewidth=2,linestyle=':')
-    plt.axvline(x=mu+cdfRange,c='black',linewidth=2,linestyle=':')
-    
-    ##lines for actual values for given sd range
-    plt.axvline(x=sampleMean-sdRange*sampleVar,c='g',linewidth=2)
-    plt.axvline(x=sampleMean+sdRange*sampleVar,c='g',linewidth=2)
-    plt.show()
-    
+for i in sample:
+    if not (i - sd_range * sigma > mu or i + sd_range * sigma < mu):
+        count += 1
+    if i > largest:
+        largest = i
+    if i < smallest:
+        smallest = i
 
+cdf_range = sigma * sd_range  ##used to calculate desired area
+# under normal curve
 
+act_percent = str((count / n) * 100) + "%"  ##actual percent of desired
+# samples within specified range
+
+# theoretical percent of desired samples within specified range
+expected_percent = str((norm(mu, sigma).cdf(mu + cdf_range) - norm(mu, sigma).cdf(mu - cdf_range)) * 100) + "%"
+
+print("Total sample size:", n)
+print("Total desired values:", count)
+print("Largest value:", largest)
+print("Smallest value:", smallest)
+print("Expected percent of values within", str(sd_range), "standard devs of the mean:", expected_percent)
+print("Actual percent of values within", str(sd_range), "standard devs of the mean:", act_percent)
+
+n, bins, patches = plt.hist(sample, 100, density=1, facecolor='b', alpha=0.7)
+(sample_mean, sample_var) = norm.fit(sample)
+y = norm.pdf(bins, sample_mean, sample_var)
+l = plt.plot(bins, y, linewidth=2, color='red')
+plt.xlabel("Value")
+plt.ylabel("Probability")
+plt.title("Histogram of Sample: $\overline{x}=%.3f,\ s=%.3f$" % (sample_mean, sample_var))
+plt.xlim([mu - 4 * sigma, mu + 4 * sigma])
+plt.grid(True)
+
+# lines for theoretical range for given sd range
+plt.axvline(x=mu - cdf_range, c='black', linewidth=2, linestyle=':')
+plt.axvline(x=mu + cdf_range, c='black', linewidth=2, linestyle=':')
+
+# lines for actual values for given sd range
+plt.axvline(x=sample_mean - sd_range * sample_var, c='g', linewidth=2)
+plt.axvline(x=sample_mean + sd_range * sample_var, c='g', linewidth=2)
+plt.show()
